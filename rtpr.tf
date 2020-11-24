8fb251c4388deb956da5eac9d2544e31c52ecd34
Tudor Ciupe <ciupe.tudor@gmail.com>
2020-11-24 07:07:32 +0000

    final working version for region us-east-1

--- rtpr.tf
+++ rtpr.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "rtpr" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_eip.nat.id}"
+    gateway_id = "${aws_nat_gateway.gw.id}"
   }
 }
 
--- sec_group.tf
+++ sec_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "vpc_task" {
   name        = "vpc_task"
-  vpc_id      = "aws_vpc.TF.id"
+  vpc_id      = "${aws_vpc.TF.id}"
   description = "Allow TLS inbound traffic"
 
   ingress {
